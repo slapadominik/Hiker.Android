ac09bf49d2c64ba38718988e4dbc94d0e3162923
Dominik Słapa <dominik.slapa@gmail.com>
2019-07-27 08:09:16 +0200

    Add bottom navigation with navigation controller

--- app/src/main/java/com/hiker/MainActivity.kt
+++ app/src/main/java/com/hiker/MainActivity.kt
@@ -2,11 +2,27 @@ package com.hiker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        bottomNavigation.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
